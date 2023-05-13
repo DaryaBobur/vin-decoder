6eba5ffe34572b956570f66d539ef51e6e3d7081
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-13 20:38:59 +0300

    Create Loader.jsx

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import { MagnifyingGlass } from  'react-loader-spinner';
+
+const Loader = () => {
+    return (    
+        <div>
+        <MagnifyingGlass 
+            visible={true} 
+            height="100" 
+            width="100"
+            color='black'
+        />
+        </div>
+    )
+}
+
+export default Loader;
