987d7232ea6cbfc85147ba1c783ed7e275726d4d
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-13 20:38:56 +0300

    Create VinList.jsx

---
+++ src/components/VinList/VinList.jsx
@@ -0,0 +1,30 @@
+import { useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const VinList = ({ items }) => {
+  const item = items.data.Results;
+  const location = useLocation();
+
+  return (
+    <ul>
+      {item.map(
+        ({ Value, Variable, VariableId }) =>
+          Value !== null &&
+          Value !== '' &&
+          Value !== '0' && (
+            <li key={VariableId}>
+              <p>
+                {Value} : {Variable}
+              </p>
+            </li>
+          )
+      )}
+    </ul>
+  );
+};
+
+VinList.propTypes = {
+  items: PropTypes.object.isRequired,
+};
+
+export default VinList;
