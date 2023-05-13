bcbb24be084e4ad974403bc9c2948a32e57bf76f
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-13 20:39:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
+import GeneralPage from 'pages/General';
+import { ToastContainer } from 'react-toastify';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      vin-decoder
+    <div>
+      <GeneralPage />
+
+      <ToastContainer autoClose={3000} theme={'colored'} />
     </div>
   );
 };
+
+export default App;
