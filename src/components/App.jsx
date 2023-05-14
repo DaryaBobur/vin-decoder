9b825da733dcb2aaee45bd6846c5583658ae7b4a
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-14 19:06:28 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,35 @@
-import GeneralPage from 'pages/General';
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
 import { ToastContainer } from 'react-toastify';
+
+const GeneralPage = lazy(() => import('../pages/General/General'));
+const VariablesPage = lazy(() => import('../pages/Variables/Variables'));
+const VariableDescription = lazy(() =>
+  import('../pages/VariableDescription/VariableDescription')
+);
+const SharedLayout = lazy(() =>
+  import('../components/SharedLayout/SharedLayout')
+);
 
 const App = () => {
   return (
-    <div>
-      <GeneralPage />
+    <>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={SharedLayout} />
+          <Route index element={<GeneralPage />} />
+          <Route path="/variables" element={<VariablesPage />}/>
+            <Route
+              path="/variables/:variableId"
+              element={<VariableDescription />}
+            />
+          <Route path="*" element={<GeneralPage />} />
+        </Routes>
+      </Suspense>
 
       <ToastContainer autoClose={3000} theme={'colored'} />
-    </div>
+    </>
   );
 };
 
