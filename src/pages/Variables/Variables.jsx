04b414321107ef48ca5b2edac619ec910f019bb3
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-14 19:06:59 +0300

    Create Variables.jsx

---
+++ src/pages/Variables/Variables.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import AppBar from 'components/AppBar/AppBar';
+import Loader from 'components/Loader/Loader';
+import VariablesList from 'components/VariablesList/VariablesList';
+import { getSearchVariablesList } from 'services/getSearchVin';
+
+
+const VariablesPage = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchVariablesList = async () => {
+      setIsLoading(true);
+      try {
+        const data = await getSearchVariablesList();
+        setItems(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchVariablesList();
+  }, []);
+
+  return (
+    <>
+      <AppBar />
+      {items.length !== 0 && <VariablesList items={items} />}
+      {isLoading && <Loader />}
+      {error && <p>Please try again later!</p>}
+    </>
+  );
+};
+
+export default VariablesPage;
