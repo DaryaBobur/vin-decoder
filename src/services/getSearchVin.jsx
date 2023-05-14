1a06bfcae0bc7e44337999dbcfd052549fc99edf
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-14 19:07:01 +0300

    Update getSearchVin.jsx

--- src/services/getSearchVin.jsx
+++ src/services/getSearchVin.jsx
@@ -8,3 +8,15 @@ export const getSearchVin = async ({ vin = '' }) => {
   );
   return result;
 };
+
+export const getSearchVariablesList = async () => {
+  const result = await axios.get(
+    `${BASE_URL}/vehicles/getvehiclevariablelist?format=json`
+  );
+  return result;
+};
+
+export const getVariableDescription = async({variableId} ) => {
+  const result = await axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablevalueslist/${variableId}?format=json`);
+  return result;
+};
