d95d2ad20e875af36bd1184f59481cf2b24b5c47
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-14 19:06:36 +0300

    Create VariablesList.jsx

---
+++ src/components/VariablesList/VariablesList.jsx
@@ -0,0 +1,29 @@
+// import PropTypes from 'prop-types';
+import { useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+
+const VariablesList = ({ items }) => {
+  const item = items.data.Results;
+    const location = useLocation();
+
+  return (
+    <ul>
+      {item.map(({ Name, Description, ID}) => (
+        <li key={ID}>
+            <NavLink state={{from: location}} to={`/variables/${ID}`}>
+          <h3>{Name}</h3>
+          </NavLink>
+          <p> {Description} </p>
+          
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+// VariablesList.propTypes = {
+//   items: PropTypes.object.isRequired,
+// };
+
+export default VariablesList;
