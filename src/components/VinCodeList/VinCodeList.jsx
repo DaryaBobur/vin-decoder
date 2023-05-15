84361bdea59879b16c77a709a331bb8ee485c1bc
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-15 22:52:42 +0300

    Update VinCodeList.jsx

--- src/components/VinCodeList/VinCodeList.jsx
+++ src/components/VinCodeList/VinCodeList.jsx
@@ -1,16 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const VinCodeList = ({ codes }) => {
-  console.log(codes);
+  const location = useLocation();
+
   return (
     <div>
-        <h2>List codes</h2>
-    <ul>
-      {codes.slice(0, 5).map(({ code, id }) => (
-        <li key={id}>
-          <p>VIN code : {code}</p>
-        </li>
-      ))}
-    </ul>
+      <h2>History codes</h2>
+      <ul>
+        {codes.slice(0, 5).map(({ code, id }) => (
+          <li key={id}>
+            <NavLink state={{ from: location }} to={`?vin=${code}`}>
+              <p><b>VIN code:</b> {code}</p>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
