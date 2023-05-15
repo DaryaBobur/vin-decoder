d1775b00f13eebc61451bac89a021ef234284fda
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-15 22:52:38 +0300

    Update VariablesList.jsx

--- src/components/VariablesList/VariablesList.jsx
+++ src/components/VariablesList/VariablesList.jsx
@@ -1,29 +1,27 @@
-// import PropTypes from 'prop-types';
-import { useLocation } from "react-router-dom";
-import { NavLink } from "react-router-dom";
-
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const VariablesList = ({ items }) => {
   const item = items.data.Results;
-    const location = useLocation();
+  const location = useLocation();
 
   return (
     <ul>
-      {item.map(({ Name, Description, ID}) => (
+      {item.map(({ Name, Description, ID }) => (
         <li key={ID}>
-            <NavLink state={{from: location}} to={`/variables/${ID}`}>
-          <h3>{Name}</h3>
+          <NavLink state={{ from: location }} to={`/variables/${ID}`}>
+            <h3>{Name}</h3>
           </NavLink>
           <p> {Description} </p>
-          
         </li>
       ))}
     </ul>
   );
 };
 
-// VariablesList.propTypes = {
-//   items: PropTypes.object.isRequired,
-// };
+VariablesList.propTypes = {
+  items: PropTypes.object.isRequired,
+};
 
 export default VariablesList;
