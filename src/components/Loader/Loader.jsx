b7b0e7002ce5ecdf18021dda6179dacb6111e707
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-15 22:52:34 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,11 @@
-import { MagnifyingGlass } from  'react-loader-spinner';
+import { MagnifyingGlass } from 'react-loader-spinner';
 
 const Loader = () => {
-    return (    
-        <div>
-        <MagnifyingGlass 
-            visible={true} 
-            height="100" 
-            width="100"
-            color='black'
-        />
-        </div>
-    )
-}
+  return (
+    <div>
+      <MagnifyingGlass visible={true} height="100" width="100" color="black" />
+    </div>
+  );
+};
 
 export default Loader;
