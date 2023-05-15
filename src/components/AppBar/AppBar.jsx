0e44bc4809abe36824fb7cf11a0121ca910c251c
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-15 22:52:29 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,32 +1,34 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 const navItems = [
-    { href: '/', text: 'General' },
-    { href: '/variables', text: 'Variables' },
-  ];
-  
-  const AppBar = () => {
-      return (
-          <header>
-              <ul>
-                  {navItems.map(({ href, text }) => {
-                      if(href.includes("/variables")) {
-                          return (
-                              <li key={href}>
-                                  <NavLink to={href}>{text}</NavLink>
-                              </li>
-                          )
-                      }    
-  
-                      return (
-                          <li key={href}>
-                              <NavLink to={href} end>{text}</NavLink>
-                          </li>
-                      )
-                  })}
-              </ul>
-          </header>
-      )
-  }
-  
-  export default AppBar;
+  { href: '/', text: 'General' },
+  { href: '/variables', text: 'Variables' },
+];
+
+const AppBar = () => {
+  return (
+    <header>
+      <ul>
+        {navItems.map(({ href, text }) => {
+          if (href.includes('/variables')) {
+            return (
+              <li key={href}>
+                <NavLink to={href}>{text}</NavLink>
+              </li>
+            );
+          }
+
+          return (
+            <li key={href}>
+              <NavLink to={href} end>
+                {text}
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </header>
+  );
+};
+
+export default AppBar;
