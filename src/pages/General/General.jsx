e59fa3934816e3da644968fcc4e1609bb4b8e897
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-15 20:35:53 +0300

    Update General.jsx

--- src/pages/General/General.jsx
+++ src/pages/General/General.jsx
@@ -1,17 +1,34 @@
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { nanoid } from 'nanoid';
 import AppBar from 'components/AppBar/AppBar';
 import FormSearchVin from 'components/FormSearchVin/FormSearchVin';
 import VinList from 'components/VinList/VinList';
 import Loader from 'components/Loader/Loader';
 import { getSearchVin } from 'services/getSearchVin';
-
+import VinCodeList from 'components/VinCodeList/VinCodeList';
 
 const GeneralPage = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [codes, setCodes] = useState(
+    JSON.parse(localStorage.getItem('codes')) ?? []
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem('codes', JSON.stringify(codes));
+  }, [codes]);
+
+  const addCode = data => {
+    const code = {
+      id: nanoid(),
+      code: data.code,
+    };
+    setCodes(prevState => [code, ...prevState]);
+    console.log(code);
+  };
 
   const vin = searchParams.get('vin');
 
@@ -39,13 +56,16 @@ const GeneralPage = () => {
 
     getSearchQuery();
   }, [vin]);
-
+  console.log(codes);
   return (
     <>
       <AppBar />
-      <FormSearchVin onSubmit={onChangeQuery} />
+      <FormSearchVin onSubmit={onChangeQuery} add={addCode} />
+      {codes.length !== 0 && <VinCodeList codes={codes} />}
       {items.length !== 0 && (
-      <div><VinList items={items} /></div>
+        <div>
+          <VinList items={items} />
+        </div>
       )}
       {isLoading && <Loader />}
       {error && <p>Please try again later!</p>}
