cdc08398df2ab43e5504d3a5f942f4a39bac7694
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-16 13:10:25 +0300

    Create VariableValue.jsx

---
+++ src/components/VariablesList/VariableValue/VariableValue.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import './VariableValue.css'
+const VariablesValue = ({ items }) => {
+  const item = items.data.Results;
+
+  if (item.length === 0) {
+    return <p className='text'>No details!</p>;
+  }
+
+  return (
+    <>
+      <h2 className='title'>All values:</h2>
+      <ul className='value-list'>
+        {item.map(({ ElementName, Name }) => (
+          <li className='value-list__item' key={Name}>
+            <p>
+              {ElementName} - {Name}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+VariablesValue.propTypes = {
+  items: PropTypes.object.isRequired,
+};
+
+export default VariablesValue;
