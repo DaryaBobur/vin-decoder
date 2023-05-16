2a8ca94eaf34c7533d311ebdb782102de84cd44d
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-16 18:25:15 +0300

    Update VariablesList.jsx

--- src/components/VariablesList/VariablesList.jsx
+++ src/components/VariablesList/VariablesList.jsx
@@ -1,19 +1,23 @@
 import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import '../VariablesList/VariablesList.css'
+import { useLocation, NavLink } from 'react-router-dom';
+import '../VariablesList/VariablesList.css';
+
 const VariablesList = ({ items }) => {
   const item = items.data.Results;
   const location = useLocation();
 
   return (
-    <ul className='variables-list'>
+    <ul className="variables-list">
       {item.map(({ Name, Description, ID }) => (
-        <li key={ID} className='variables-list__item'>
-          <NavLink className='variables-list__link' state={{ from: location }} to={`/variables/${ID}`}>
-            <h3 className='name-var'>{Name}</h3>
+        <li key={ID} className="variables-list__item">
+          <NavLink
+            className="variables-list__link"
+            state={{ from: location }}
+            to={`/variables/${ID}`}
+          >
+            <h3 className="name-var">{Name}</h3>
           </NavLink>
-          <p className='description-var'> {Description} </p>
+          <p className="description-var"> {Description} </p>
         </li>
       ))}
     </ul>
