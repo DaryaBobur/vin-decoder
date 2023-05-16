038ad84fde8a4b694fe5327e677fa17e65655183
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-05-16 18:25:21 +0300

    Update VariableValue.jsx

--- src/components/VariablesList/VariableValue/VariableValue.jsx
+++ src/components/VariablesList/VariableValue/VariableValue.jsx
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types';
-import './VariableValue.css'
+import './VariableValue.css';
+
 const VariablesValue = ({ items }) => {
   const item = items.data.Results;
 
   if (item.length === 0) {
-    return <p className='text'>No details!</p>;
+    return <p className="text">No details!</p>;
   }
 
   return (
     <>
-      <h2 className='title'>All values:</h2>
-      <ul className='value-list'>
+      <h2 className="title">All values:</h2>
+      <ul className="value-list">
         {item.map(({ ElementName, Name }) => (
-          <li className='value-list__item' key={Name}>
+          <li className="value-list__item" key={Name}>
             <p>
               {ElementName} - {Name}
             </p>
